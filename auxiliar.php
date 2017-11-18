20c30e89752c9374c2e00c077b66a1caf6dfb9d0
Jose Arjona <arjonatorres79@gmail.com>
2017-11-18 10:41:13 +0100

    Añadidas más funciones

--- auxiliar.php
+++ auxiliar.php
@@ -26,10 +26,6 @@ function selected(?string $o, string $v): string
     return $o === $v ? 'selected': '';
 }
 
-
-$op1 = $op2 = $op = null;
-extract($_GET, EXTR_IF_EXISTS);
-
 function calcula($op1, $op2, $op)
 {
     eval("\$ret = $op1 $op $op2;"); //Es peligroso, no usar.
@@ -48,4 +44,71 @@ function calcula($op1, $op2, $op)
     //         break;
     // }
     return $ret;
+}
+
+function compruebaParametros($op1, $op2, $op, array &$error): void
+{
+    // Si los tres parámetros están en la URL...
+    if(isset($op1, $op2, $op)) {
+        // Si algún operando es vacío...
+        if ($op1 === '' || $op2 === '') {
+            $error[] = 'Falta algún parámetro';
+            throw new Exception;
+        }
+        // Están los tres parámetros y no son vacíos
+        return;
+    }
+    // Si falta algún parámetro en la URL, pero no todos...
+    if ($op1 !== null || $op2 !== null || $op !== null) {
+        $error[] = 'Falta algún parámetro';
+    }
+    // Si falta todos los parámetros en la URL
+    throw new Exception;
+}
+
+function compruebaOperador($op, array $lista, array &$error): void
+{
+    if (!in_array($op, $lista)) {
+        $error[] = 'Operación no válida';
+    }
+}
+
+function compruebaOperandos($op1, $op2, array &$error): void
+{
+    if (!is_numeric($op1) || !is_numeric($op2)) {
+        $error[] = 'Los dos operandos deben ser numéricos';
+    } elseif ($op1 < 0 || $op2 < 0) {
+        $error[] = 'Los dos operandos deben ser positivos';
+    }
+}
+
+function compruebaErrores(array $error)
+{
+    if (!empty($error)) {
+        throw new Exception;
+    }
+}
+
+function mostrarErrores($error)
+{
+    foreach ($error as $v):
+    ?>
+        <h3>Error: <?= $v ?>.</h3>
+    <?php
+    endforeach;
+}
+
+function dibujaFormulario($op1, $op2, $op, array $lista): void
+{
+?>
+    <form action="calcula.php" method="get">
+        <label for="op1">Primer operando:</label>
+        <input id="op1" type="text" name="op1" value="<?= htmlentities($op1) ?>" /><br />
+        <label for="op2">Segundo operando:</label>
+        <input id="op2" type="text" name="op2" value="<?= htmlentities($op2) ?>" /><br />
+        <label for="op">Operación:</label>
+        <?php seleccion($lista, $op, 'op') ?><br />
+        <input type="submit" value="Calcular" />
+    </form>
+<?php
 }
